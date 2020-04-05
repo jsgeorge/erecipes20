c67a59392bbb9ccb9c27d9b9214c502c46c9f9b7
George M <gmeyer1000@yahoo.com>
2020-04-05 12:40:45 -0700

    modified build in index.js

--- server/index.js
+++ server/index.js
@@ -28,25 +28,25 @@ app.use("/users/favorite", users);
 
 const port = process.env.PORT || 3002;
 
-// if (process.env.NODE_ENV === "production") {
-//   //Exprees will serve up production assets
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  //Exprees will serve up production assets
+  app.use(express.static("client/build"));
 
 //Express serve up index.html file if it doesn't recognize route
-//const path = require("path");
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
-//   app.get("*", function(_, res) {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"), function(
-//       err
-//     ) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-//     });
-//   });
-//}
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+  app.get("*", function(_, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"), function(
+      err
+    ) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
